0a34ad2d78191d9f5e6367b3b8add2020fb793cd
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2022-03-08 23:19:46 -0500

    Update assets

--- public/dist/assets/About.285d6f64.js
+++
@@ -1,1 +0,0 @@
-import{b as o}from"./app.59b1b0f9.js";import{o as t,a as e}from"./vendor.80495a89.js";const r={},c={class:"text-2xl font-bold"};function s(a,n){return t(),e("h1",c,"About")}var l=o(r,[["render",s]]);export{l as default};
